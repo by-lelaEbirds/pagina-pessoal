bb37d04237d985b6e25d14734287f0bbeb881402
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-11-13 09:09:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- CONFIGURAÇÃO ---
     // ATENÇÃO: Vamos preencher isso no final, depois do deploy do backend
-    const BACKEND_URL = "https://portfolio-backend-ts0d.onrender.com/"; 
+    const BACKEND_URL = "https://portfolio-backend-ts0d.onrender.com";
 
     // --- ELEMENTOS DO DOM ---
     const loginScreen = document.getElementById('login-screen');
